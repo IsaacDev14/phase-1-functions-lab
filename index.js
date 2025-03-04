5e85996a77ef5f9025f8c3b4fd20f631d8d2b783
IsaacDev14 <mwitiisaac14@gmail.com>
2025-03-04 21:00:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    const headQ = 42 ;
+    return Math.abs(distance - headQ)
+}
+
+
+function distanceFromHqInFeet(distance){
+    const headQ = 42;
+    return Math.abs((distance - headQ)*264)
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((destination - start)*264)
+}
+
+function calculatesFarePrice(start, destination){
+    const feetPrice = Math.abs((destination - start)*264)
+    if(feetPrice < 400 ) {
+        return 0;
+    } else if(feetPrice > 400 && feetPrice <= 2000){
+        return 2.56;
+    }else if(feetPrice > 2000 && feetPrice <= 2500){
+        return 25;
+    } else{
+        return 'cannot travel that far';
+    }
+}
